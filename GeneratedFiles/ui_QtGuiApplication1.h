7d873b76be900aa1170588fb72456c9c62fdac89
Álvaro Lázaro Sevilla <alseether@users.noreply.github.com>
2017-11-14 09:29:22 +0100

    Added some config and auto generated files

---
+++ GeneratedFiles/ui_QtGuiApplication1.h
@@ -0,0 +1,137 @@
+/********************************************************************************
+** Form generated from reading UI file 'QtGuiApplication1.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_QTGUIAPPLICATION1_H
+#define UI_QTGUIAPPLICATION1_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include "MyCanvas.hpp"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_QtGuiApplication1Class
+{
+public:
+    QWidget *centralWidget;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QPlainTextEdit *plainTextEdit;
+    MyCanvas *widget;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *QtGuiApplication1Class)
+    {
+        if (QtGuiApplication1Class->objectName().isEmpty())
+            QtGuiApplication1Class->setObjectName(QStringLiteral("QtGuiApplication1Class"));
+        QtGuiApplication1Class->resize(600, 400);
+        centralWidget = new QWidget(QtGuiApplication1Class);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        horizontalLayoutWidget = new QWidget(centralWidget);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(0, 290, 601, 51));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton_2 = new QPushButton(horizontalLayoutWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+
+        horizontalLayout->addWidget(pushButton_2);
+
+        pushButton_3 = new QPushButton(horizontalLayoutWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+
+        horizontalLayout->addWidget(pushButton_3);
+
+        pushButton = new QPushButton(horizontalLayoutWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        horizontalLayout->addWidget(pushButton);
+
+        verticalLayoutWidget = new QWidget(centralWidget);
+        verticalLayoutWidget->setObjectName(QStringLiteral("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(0, 0, 601, 281));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        scrollArea = new QScrollArea(verticalLayoutWidget);
+        scrollArea->setObjectName(QStringLiteral("scrollArea"));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QStringLiteral("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 597, 277));
+        plainTextEdit = new QPlainTextEdit(scrollAreaWidgetContents);
+        plainTextEdit->setObjectName(QStringLiteral("plainTextEdit"));
+        plainTextEdit->setGeometry(QRect(0, 0, 591, 271));
+        widget = new MyCanvas(scrollAreaWidgetContents);
+        widget->setObjectName(QStringLiteral("widget"));
+        widget->setGeometry(QRect(0, 0, 1031, 611));
+        scrollArea->setWidget(scrollAreaWidgetContents);
+
+        verticalLayout->addWidget(scrollArea);
+
+        QtGuiApplication1Class->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(QtGuiApplication1Class);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 21));
+        QtGuiApplication1Class->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(QtGuiApplication1Class);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        QtGuiApplication1Class->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(QtGuiApplication1Class);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        QtGuiApplication1Class->setStatusBar(statusBar);
+
+        retranslateUi(QtGuiApplication1Class);
+
+        QMetaObject::connectSlotsByName(QtGuiApplication1Class);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *QtGuiApplication1Class)
+    {
+        QtGuiApplication1Class->setWindowTitle(QApplication::translate("QtGuiApplication1Class", "QtGuiApplication1", 0));
+        pushButton_2->setText(QApplication::translate("QtGuiApplication1Class", "PushButton", 0));
+        pushButton_3->setText(QApplication::translate("QtGuiApplication1Class", "PushButton", 0));
+        pushButton->setText(QApplication::translate("QtGuiApplication1Class", "PushButton", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class QtGuiApplication1Class: public Ui_QtGuiApplication1Class {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_QTGUIAPPLICATION1_H
